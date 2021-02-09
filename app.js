623492802d13f4c93de353f64089c72c54764da7
JamiePMortimer <16468666+JamiePMortimer@users.noreply.github.com>
2021-02-09 20:20:09 +0000

    Amended section classes

--- app.js
+++ app.js
@@ -1,16 +1,25 @@
 const menuSelect = document.querySelectorAll('.nav-box');
+const mainSelect = document.querySelector('.centre-main');
+const mainSection = mainSelect.querySelectorAll('.main-section');
 
-menuSelect.forEach(e => {
-  e.addEventListener('click', ()=>{
-      e.classList.remove('inactive')
-      e.classList.add('active')
-      menuSelect.forEach(ev => {
-        if(ev === e){
-          return
-        } else {
-          ev.classList.add('inactive')
-          ev.classList.remove('active')
-        }
-      })
-  })
-})
+menuSelect.forEach((e) => {
+  e.addEventListener('click', () => {
+    e.removeAttribute('inactive');
+    e.classList.add('active');
+    menuSelect.forEach((ev) => {
+      if (ev === e) {
+        return;
+      } else {
+        ev.classList.add('inactive');
+        ev.classList.remove('active');
+      }
+    });
+  });
+  // mainSection.forEach((evnt) => {
+  //   if (evnt.classList[1] === e.classList[1]) {
+  //     evnt.removeAttribute('hide')
+  //   } else {
+  //     evnt.classList.add('hide');
+  //   }
+  // });
+});
