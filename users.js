2c1564d025474fbb04b642b7cd066a14d324eb00
JamiePMortimer <16468666+JamiePMortimer@users.noreply.github.com>
2021-02-11 20:01:54 +0000

    Added render method for users

--- users.js
+++ users.js
@@ -12,4 +12,21 @@ class UserList {
     new User('Hugo', 8, '/images/Kyle.JPG'),
     new User('Nate', 10, '/images/Kyle.JPG'),
   ];
+  constructor(){};
+
+  render(){
+    const renderHook = document.getElementById('test');
+    const userList = document.createElement('ul');
+    userList.className = 'user-list';
+    for (const user of this.users) {
+      const userEl = document.createElement('li');
+      userEl.className = 'user-item';
+      userEl.innerHTML = `
+      <div class="item">
+        <img src="${user.image}" width="150px" alt="${user.name}">
+
+      </div>
+      `
+    }
+  }
 }
